e1fb75b30bae60aacc468df5ca6e34a66020d7dc
SoniaPunjabi <64728477+SoniaPunjabi@users.noreply.github.com>
2021-06-07 06:25:09 +0530

    Add files via upload

---
+++ 3_Implementation/src/getWord.c
@@ -0,0 +1,13 @@
+#include"headers.h"
+char* getWord(int randomIndex)
+{char* guessWords[] = {
+		"passport", 
+		"vaccine",
+		"cheesecake",
+		"netflix",
+		"documentary",
+		"potato"	
+	};
+	
+	return (guessWords[randomIndex]);}
+	
